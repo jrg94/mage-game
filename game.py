552371dcf0a4a2477f72b9be62083f5417459374
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-17 00:50:12 -0400

    Following the real python tutorial

--- game.py
+++ game.py
@@ -1,3 +1,70 @@
+# Simple pygame program
+
+# Import and initialize the pygame library
 import pygame
 
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Define a Player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        
+# Initialize pygame
 pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instantiate player. Right now, this is just a rectangle.
+player = Player()
+
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+            
+    # Get the set of keys pressed and check for user input
+    pressed_keys = pygame.key.get_pressed()
+    
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
+
+    # Draw the player on the screen
+    screen.blit(player.surf, player.rect)
+
+    # Update the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
