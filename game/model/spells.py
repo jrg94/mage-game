e1a70e0e54a3672ebd91c39aef376320bc0133ee
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-19 13:33:38 -0400

    Fleshed out projectile code

--- game/model/spells.py
+++ game/model/spells.py
@@ -34,22 +34,27 @@ class Projectile:
     """
     A projectile-based spell.
 
-    :param element: the element of the spell.
-    :param speed_level: the speed level of the spell.
-    :param radius_level: the radius level of the spell.
-    :param distance_level: the distance level of the spell.
-    :param damage_level: the damage level of the spell.
-    :cvar BASE_SPEED: the base speed of the spell in meters per second.
-    :cvar BASE_RADIUS: the base radius of the spell in meters.
+    :param Element element: the element of the spell.
+    :param int speed_level: the speed level of the spell.
+    :param int radius_level: the radius level of the spell.
+    :param int distance_level: the distance level of the spell.
+    :param int damage_level: the damage level of the spell.
+    :cvar float BASE_SPEED: the base speed of the spell in meters per second.
+    :cvar float BASE_RADIUS: the base radius of the spell in meters.
+    :cvar float BASE_DISTANCE: the base distance of the spell in meters.
+    :cvar int BASE_DAMAGE: the base damage of the spell in hit points.
     """
 
     element: Element = Element.NONE
     speed_level: int = 1
     radius_level: int = 1
     distance_level: int = 1
+    damage_level: int = 1
 
     BASE_SPEED: ClassVar[float] = 5.0
     BASE_RADIUS: ClassVar[float] = .1
+    BASE_DISTANCE: ClassVar[float] = 10.0
+    BASE_DAMAGE: ClassVar[int] = 1
 
     @staticmethod
     def scale(level: int, base: float) -> float:
@@ -76,3 +81,9 @@ class Projectile:
 
     def radius(self) -> float:
         return self.scale(self.radius_level, self.BASE_RADIUS)
+    
+    def distance(self) -> float:
+        return self.scale(self.distance_level, self.BASE_DISTANCE)
+    
+    def damage(self) -> float:
+        return self.scale(self.damage_level, self.BASE_DAMAGE)
