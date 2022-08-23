869d2fedaa34e925a1f3830379e91238054a50f8
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-23 17:28:20 -0400

    Cleaning up code

--- test/test_attribute_tracking.py
+++ test/test_attribute_tracking.py
@@ -1,12 +1,15 @@
 import math
+
 from mage_game import model
+
 
 def test_tracking_level_1():
     tracking = model.AttributeTracking(model.SpellAttribute.DISTANCE, 10)
     assert tracking.level() == 1, "Default level should be 1"
     assert tracking.effective_value() == 10, "Provided distance should be 10: log2(1) * 10 + 10"
     assert tracking.events_to_next_level() == 10, "Default event count should be 10: ((2 ^ 2) - 2) * 5 - 0"
-    
+
+
 def test_tracking_level_2():
     tracking = model.AttributeTracking(model.SpellAttribute.DISTANCE, 10)
     while tracking.level() != 2:
@@ -14,7 +17,8 @@ def test_tracking_level_2():
     assert tracking.level() == 2, "Tracker should have reached level 2"
     assert tracking.effective_value() == 20, "Computed value should be 20: log2(2) * 10 + 10"
     assert tracking.events_to_next_level() == 20, "Computed value should be 20: ((2 ^ 3) - 2) * 5 - 10"
-    
+
+
 def test_tracking_level_3():
     tracking = model.AttributeTracking(model.SpellAttribute.DISTANCE, 10)
     while tracking.level() != 3:
