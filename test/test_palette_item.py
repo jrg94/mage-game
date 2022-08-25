6a0fdf319e9a4c8b4a46f09f6b8d72838b4faa53
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-24 21:03:16 -0400

    Cleaning up tests

--- test/test_palette_item.py
+++ test/test_palette_item.py
@@ -2,10 +2,10 @@ from mage_game.model import *
 
 def test_palette_item_default():
     default = PaletteItem()
-    assert default.can_use() == True
-    assert default.get_spell() == Projectile()
+    assert default.can_use() == True, "Default palette items should be usable from the start"
+    assert default.get_spell() == Projectile(), "Default palette items should store default projectiles"
     
 def test_palette_item_reset_cooldown():
     default = PaletteItem()
     default.reset_cooldown()
-    assert default.get_cooldown() == SpellAttribute.COOLDOWN.base_value
+    assert default.get_cooldown() == SpellAttribute.COOLDOWN.base_value, "Default palette items should store the default cooldown after reset"
