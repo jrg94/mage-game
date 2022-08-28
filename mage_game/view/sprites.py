4a15b102bd67e37bc99bb755a53bc8e6fcd4c3ba
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-28 15:16:41 -0400

    Fixed particle position

--- mage_game/view/sprites.py
+++ mage_game/view/sprites.py
@@ -212,9 +212,10 @@ class ProjectileSprite(pygame.sprite.Sprite):
         """
         particle_count = (self.size[0] * self.size[1]) // 20
         particle_color = self._get_adjacent_color(self.source.element().color, random.randint(25, 35))
+        camera = self.rect.topleft - self.camera_group.offset
         for _ in range(particle_count):
-            x = random.randint(self.rect.topleft[0], self.rect.topleft[0] + self.size[0])
-            y = random.randint(self.rect.topleft[1], self.rect.topleft[1] + self.size[1])
+            x = random.randint(camera[0], camera[0] + self.size[0])
+            y = random.randint(camera[1], camera[1] + self.size[1])
             pygame.draw.circle(
                 pygame.display.get_surface(),
                 particle_color,
