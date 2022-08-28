905cb2d52639e7a7ef9a0bb1f468e1fb1f5eea0f
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-28 17:26:07 -0400

    Cleaned up main code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from logging.handlers import RotatingFileHandler
 
 from mage_game import controller, eventmanager, model, view
@@ -7,8 +8,12 @@ from mage_game import controller, eventmanager, model, view
 logger = logging.getLogger(__name__)
 
 
-def _init_logger():
-    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "logs")
+def _init_logger(log_path: str) -> None:
+    """
+    Initializes the logger from a log path.
+
+    :param log_path: the log path to dump logs
+    """
     os.makedirs(log_path, exist_ok=True)
     log_file_path = os.path.join(log_path, "game.log")
     logging.basicConfig(
@@ -25,20 +30,32 @@ def _init_logger():
     logger.info(f"Initialized logger: files dumped to {log_path}")
 
 
-def _run_with_crash_handling(game_model: model.GameEngine):
+def _run_with_crash_handling(game_model: model.GameEngine, log_path: str):
+    """
+    Runs the game while waiting for errors.
+
+    :param game_model: the game engine that runs the game
+    :param log_path: the path to the log to print in case of a crash
+    """
     try:
         game_model.run()
     except Exception as e:
         logger.exception(e)
+        print(f"Game crashed! Check logs: {log_path}")
+        sys.exit(1)
 
 
 def run():
-    _init_logger()
+    """
+    The main function that launches the game.
+    """
+    log_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "logs")
+    _init_logger(log_path)
     event_manager = eventmanager.EventManager()
     game_model = model.GameEngine(event_manager)
     keyboard = controller.MouseAndKeyboard(event_manager, game_model)
     graphics = view.GraphicalView(event_manager, game_model)
-    _run_with_crash_handling(game_model)
+    _run_with_crash_handling(game_model, log_path)
 
 
 if __name__ == '__main__':
