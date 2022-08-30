d71d65a507a59206de5594c21915a0585a75d2ad
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-08-29 23:59:17 -0400

    Cleaning up some of the code

--- mage_game/controller.py
+++ mage_game/controller.py
@@ -9,13 +9,13 @@ class MouseAndKeyboard:
     Handles keyboard input.
     """
 
-    def __init__(self, evManager: EventManager, model: GameEngine):
+    def __init__(self, event_manager: EventManager, model: GameEngine):
         """
         evManager (EventManager): Allows posting messages to the event queue.
         model (GameEngine): a strong reference to the game Model.
         """
-        self.evManager = evManager
-        evManager.register_listener(self)
+        self.event_manager = event_manager
+        event_manager.register_listener(self)
         self.model = model
 
     def notify(self, event):
@@ -28,7 +28,7 @@ class MouseAndKeyboard:
             for event in pygame.event.get():
                 # handle window manager closing our window
                 if event.type == pygame.QUIT:
-                    self.evManager.post(QuitEvent())
+                    self.event_manager.post(QuitEvent())
                 # handle key down events
                 if event.type == pygame.KEYDOWN:
                     currentstate = self.model.state.peek()
@@ -53,11 +53,11 @@ class MouseAndKeyboard:
         """
 
         if event.key == pygame.K_ESCAPE:
-            self.evManager.post(QuitEvent())
+            self.event_manager.post(QuitEvent())
         if event.key == pygame.K_TAB:
-            self.evManager.post(StateChangeEvent(None))
+            self.event_manager.post(StateChangeEvent(None))
         if event.key == pygame.K_SPACE:
-            self.evManager.post(StateChangeEvent(GameState.STATE_PLAY))
+            self.event_manager.post(StateChangeEvent(GameState.STATE_PLAY))
 
     def key_down_help(self, event: pygame.event.Event):
         """
@@ -65,7 +65,7 @@ class MouseAndKeyboard:
         """
 
         if event.key in [pygame.K_ESCAPE, pygame.K_SPACE, pygame.K_RETURN]:
-            self.evManager.post(StateChangeEvent(None))
+            self.event_manager.post(StateChangeEvent(None))
 
     def key_down_play(self, event: pygame.event.Event):
         """
@@ -75,11 +75,11 @@ class MouseAndKeyboard:
         the event manager to be used by the view directly.
         """
         if event.key == pygame.K_TAB:
-            self.evManager.post(StateChangeEvent(GameState.STATE_MENU))
+            self.event_manager.post(StateChangeEvent(GameState.STATE_MENU))
         elif event.key == pygame.K_F1:
-            self.evManager.post(StateChangeEvent(GameState.STATE_HELP))
+            self.event_manager.post(StateChangeEvent(GameState.STATE_HELP))
         else:
-            self.evManager.post(KeyboardEvent(event.key, event.unicode))
+            self.event_manager.post(KeyboardEvent(event.key, event.unicode))
             
     def key_down_intro(self, event: pygame.event.Event):
         """
@@ -89,7 +89,7 @@ class MouseAndKeyboard:
         :param event: the key press event
         """
         if event.key == pygame.K_ESCAPE:
-            self.evManager.post(StateChangeEvent(None))
+            self.event_manager.post(StateChangeEvent(None))
 
     def mouse_down_play(self, event: pygame.event.Event):
         """
@@ -97,7 +97,7 @@ class MouseAndKeyboard:
         
         :param event: the mouse press event object
         """
-        self.evManager.post(MouseEvent(event.button, event.pos))
+        self.event_manager.post(MouseEvent(event.button, event.pos))
         
     def mouse_down_intro(self, event: pygame.event.Event):
         """
@@ -105,4 +105,4 @@ class MouseAndKeyboard:
 
         :param event: the mouse press event object
         """
-        self.evManager.post(MouseEvent(event.button, event.pos))
+        self.event_manager.post(MouseEvent(event.button, event.pos))
